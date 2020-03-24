575877ad8306f4d97c8ae566e8df0f8107b2e177
Kobe Bogaerts <kobe.bogaerts@student.ap.be>
2020-03-24 12:51:47 +0100

    bullet vergroot en shooting script verbeterd

--- Assets/PlayerShoot.cs
+++ Assets/PlayerShoot.cs
@@ -6,17 +6,23 @@ public class PlayerShoot : MonoBehaviour
 {
 
     public GameObject bullet;
-    public float shootRate = 3f;
+    public float shootRate = 1.5f;
 
-    private float timer = 3f;
+    private float timer = 1.5f;
     private bool start;
+
+    void Start()
+    {
+        timer = shootRate;    
+    }
+
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Space) && timer >= shootRate)
         {
             GameObject copyBullet = Instantiate(bullet, transform.position + transform.forward, bullet.gameObject.transform.rotation);
-            copyBullet.GetComponent<Rigidbody>().AddForce(transform.forward * 100, ForceMode.Acceleration);
+            copyBullet.GetComponent<Rigidbody>().AddForce(transform.forward * 500, ForceMode.VelocityChange);
             start = true;
             timer = 0f;
         }
